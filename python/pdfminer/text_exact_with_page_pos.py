cf1d4585287ffef898d873331922a90e83fb2b58
Fisher Yu <12823863+szufisher@users.noreply.github.com>
2019-10-15 16:20:35 +0800

    Create text_exact_with_page_pos.py

---
+++ python/pdfminer/text_exact_with_page_pos.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from pdfminer.pdfdocument import PDFDocument, PDFNoOutlines
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LTPage, LTChar, LTAnno, LAParams, LTTextBox, LTTextLine
+from pprint import pprint
+from pdfminer.pdfpage import PDFPage
+import operator
+
+
+class PDFPageDetailedAggregator(PDFPageAggregator):
+    def __init__(self, rsrcmgr, pageno=1, laparams=None):
+        PDFPageAggregator.__init__(self, rsrcmgr, pageno=pageno, laparams=laparams)
+        self.rows = []
+        self.page_number = 0
+    def receive_layout(self, ltpage):        
+        def render(item, page_number):
+            if isinstance(item, LTPage) or isinstance(item, LTTextBox):
+                for child in item:
+                    render(child, page_number)
+            elif isinstance(item, LTTextLine):
+                child_str = ''
+                for child in item:
+                    if isinstance(child, (LTChar, LTAnno)):
+                        child_str += child.get_text()
+                child_str = ' '.join(child_str.split()).strip()
+                if child_str:
+                    row = (page_number, item.bbox[0], item.bbox[1], item.bbox[2], item.bbox[3], child_str) # bbox == (x1, y1, x2, y2)
+                    self.rows.append(row)
+                for child in item:
+                    render(child, page_number)
+            return
+        render(ltpage, self.page_number)
+        self.page_number += 1
+        self.rows = sorted(self.rows, key = lambda x: (x[0], -x[2], x[1]))   #sort by page, y, x         
+        self.result = ltpage
+
+def main(file):
+    fp = open(file, 'rb')
+    parser = PDFParser(fp)
+    doc = PDFDocument(parser)
+    #doc.initialize() # leave empty for no password
+
+    rsrcmgr = PDFResourceManager()
+    laparams = LAParams()
+    device = PDFPageDetailedAggregator(rsrcmgr, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+        # receive the LTPage object for this page
+        device.get_result()
+
+    pprint(device.rows)
+
+if __name__ == "__main__":
+    main('sample.pdf')
