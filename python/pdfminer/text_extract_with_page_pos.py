a2fc930d15d979220ac01a73881a554820ada268
Fisher Yu <12823863+szufisher@users.noreply.github.com>
2019-10-16 17:28:23 +0800

    Update text_extract_with_page_pos.py

--- python/pdfminer/text_extract_with_page_pos.py
+++ python/pdfminer/text_extract_with_page_pos.py
@@ -53,6 +53,51 @@ def main(file):
         device.get_result()
 
     pprint(device.rows)
-
+    
+    extract_data(device.rows)
+    
+def extract_data(rows):
+    header_fields = ['Order number 订单号', 'Order date 订单日期']
+    item_fields=['描述','币别']
+    table_end_row_indicators = ['Company Address:','Total value of the order without VAT']
+    header = {}
+    item=[]
+    cur_page = -1
+    new_page = False
+    row_count= -1
+    for idx, val in enumerate(rows):
+        if val[0] != cur_page:
+            new_page = True
+            item_row_started = False
+            item_fields=['描述','币别']
+            cur_page = val[0]
+        value = val[-1]
+        if header_fields and value in header_fields:
+            header.update({value: rows[idx+1][-1]})
+            header_fields.remove(value)
+        if item_fields and value == item_fields[0] and rows[idx+1][-1] == item_fields[1]:
+            item_fields = []
+            item_row_started = True
+            item_start_row = idx + 3
+            cur_row_idx = -1
+            
+        if item_row_started:                        
+            if  value in table_end_row_indicators:  #with only table header column, no content rows on 1st page
+                item_row_started = False                
+            elif idx - item_start_row >=0:
+                if len(value) == 5 and value.isnumeric():  #if len(value) == 5 and value.isnumeric():                                
+                    item.append([value])                    
+                    row_count += 1
+                    col_num=0
+                else:
+                    col_num += 1
+                    if col_num < 6 or not all(x in value for x in ['Page','of']): # remove the trailing page counter
+                        item[row_count].append(value)            
+                    
+    print(header)
+    pprint(item)
+    
+        
 if __name__ == "__main__":
-    main('sample.pdf')
+    for f in ['sample.pdf','po1.pdf','po2.pdf','po3.pdf','po4.pdf','po5.pdf']: #
+        main(f)    
